7f75f3b376cddcd65d5b1d6213720913dd17fe83
Vansh verma <147131980+vanshA12@users.noreply.github.com>
2023-12-13 21:18:45 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,17 @@
+//Using Subtraction for Addition!
+#include <stdio.h>
+
+int main() {
+   int num1, num2, sum;
+
+   printf("Enter first number:");
+   scanf("%d", &num1);
+
+   printf("Enter second number:");
+   scanf("%d", &num2);
+
+   sum = num1 - (-num2);
+   printf("Sum is : %d",sum);
+
+   return (0);
+}
